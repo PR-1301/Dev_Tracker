fc2fff3b64ae24ec92a20bf97d48f51888ee18c8
PR <ponnurajan.1314@gmail.com>
2026-06-28 13:53:23 +0530

    feat: server.js

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,31 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
 const morgan = require('morgan');
+const errorHandler = require('./middleware/errorHandler');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express();
 
 app.use(cors());
 app.use(morgan('dev'));
+app.use(express.json());
 
-app.listen(3000, ()=>{
-    console.log("Server running on PORT 3000");
+//Routes
+
+
+
+//404 error handler
+app.use((req, res) => {
+  res.status(404).json({
+    success: false,
+    message: `Route ${req.method} ${req.path} not found`,
+  });
+});
+
+//Global Error handler
+app.use(errorHandler);
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`Server running on PORT ${process.env.PORT}`);
 })
